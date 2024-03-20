1ccff6abc1f7b535752c76d0bbdf5510c6f4bd7f
Rhamanda Lima <cathyannarhamanda@gmail.com>
2024-03-19 22:26:20 -0300

    fea: add unit tests

---
+++ src/mappers/test_currency_mapper.py
@@ -0,0 +1,24 @@
+import unittest
+from mappers.CurrencyMapper import CurrencyMapper, CurrencyRepresentationRequest, CurrencyRepresentationResponse, Currency
+
+class TestCurrencyMapper(unittest.TestCase):
+    def test_map_request_to_entity(self):
+        request_data = CurrencyRepresentationRequest(name="USD", symbol="$")
+
+        result = CurrencyMapper.map_request_to_entity(request_data)
+
+        self.assertEqual(result.id, None)
+        self.assertEqual(result.name, "USD")
+        self.assertEqual(result.symbol, "$")
+
+    def test_map_entity_to_response(self):
+        currency = Currency(id=1, name="USD", symbol="$")
+
+        result = CurrencyMapper.map_entity_to_response(currency)
+
+        self.assertEqual(result.id, 1)
+        self.assertEqual(result.name, "USD")
+        self.assertEqual(result.symbol, "$")
+
+if __name__ == '__main__':
+    unittest.main()
