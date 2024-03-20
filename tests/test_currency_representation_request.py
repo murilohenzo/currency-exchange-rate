fe2595ef65751ed0361bc13c19049501c34815ea
Rhamanda Lima <cathyannarhamanda@gmail.com>
2024-03-20 19:48:29 -0300

    feature: remove comments

--- tests/test_currency_representation_request.py
+++ tests/test_currency_representation_request.py
@@ -3,14 +3,11 @@ from src.adapters.representation.CurrencyRepresentationRequest import CurrencyRe
 
 class TestCurrencyRepresentationRequest(unittest.TestCase):
     def test_init(self):
-        # Test data
         name = "USD"
         symbol = "$"
 
-        # Criar uma instância de CurrencyRepresentationRequest
         request = CurrencyRepresentationRequest(name, symbol)
 
-        # Verificar se os atributos são definidos corretamente
         self.assertEqual(request.name, name)
         self.assertEqual(request.symbol, symbol)
 
--- tests/test_sql_alchemy_currency_repository.py
+++ tests/test_sql_alchemy_currency_repository.py
@@ -13,54 +13,40 @@ class TestSQLAlchemyCurrencyRepository(unittest.TestCase):
         self.repo = SQLAlchemyCurrencyRepository(self.mock_session)
 
     def test_find_by_id(self):
-        # Mocking data
         expected_currency = Currency(id=1, name='USD', symbol='$')
 
-        # Mocking session.query().filter_by().first() method
         self.mock_session.query.return_value.filter_by.return_value.first.return_value = expected_currency
 
-        # Test
         result = self.repo.find_by_id(1)
 
-        # Assertion
         self.assertEqual(result, expected_currency)
 
     def test_find_all(self):
-        # Mocking data
         expected_currencies = [Currency(id=1, name='USD', symbol='$'), Currency(id=2, name='EUR', symbol='EUR')]
 
-        # Mocking session.query().all() method
         self.mock_session.query.return_value.all.return_value = expected_currencies
 
-        # Test
         result = self.repo.find_all()
 
-        # Assertion
         self.assertEqual(result, expected_currencies)
 
     def test_create(self):
-        # Mocking data
         currency = Currency(id=1, name='USD', symbol='$')
 
-        # Mocking session.add() and session.commit() methods
         self.mock_session.add.return_value = None
         self.mock_session.commit.return_value = None
 
-        # Test
         result = self.repo.create(currency)
 
-        # Assertion
         self.assertEqual(result, currency)
 
     def test_create_with_integrity_error(self):
-        # Mocking data
+    
         currency = Currency(id=1, name='USD', symbol='$')
 
-        # Mocking session.add() and session.commit() methods to raise IntegrityError
         self.mock_session.add.side_effect = IntegrityError(MagicMock(), MagicMock(), MagicMock())
         
         result = self.repo.create(currency)
 
-        # Assertion
         self.assertIsNone(result)
         self.mock_session.rollback.assert_called_once()
